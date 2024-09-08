f9b3350a3d4c4da5d532283028101488afa32bb3
Rishabh-tiwari0 <htrishabh.tiwari@gmail.com>
2024-09-08 09:18:42 +0530

    lu nort working so using ri

--- src/app/Components/add_to_cart_button/Button.jsx
+++ src/app/Components/add_to_cart_button/Button.jsx
@@ -1,11 +1,11 @@
 "use client";
 import React from "react";
-import { LuShoppingCart } from "react-icons/lu";
+import { RiShoppingCart2Line } from "react-icons/ri";
 
 const Button = () => {
   return (
     <button className="bg-orange-400 hover:bg-orange-500 w-full py-3 px-3 rounded-lg font-semibold flex gap-2 justify-center items-center mt-5">
-      <LuShoppingCart size={20} />
+      <RiShoppingCart2Line size={20} />
       <span>Add to cart</span>{" "}
     </button>
   );
